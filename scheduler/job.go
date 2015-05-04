d2cd2741d5b6e2270766dca6a28d74bcf7ebe91c
Michael Owings <mowings@turbosquid.com>
2015-05-04 11:07:19 -0500

    first pass remote exec

--- scheduler/job.go
+++ scheduler/job.go
@@ -5,6 +5,7 @@ import (
 	"github.com/mowings/scylla/config"
 	"github.com/mowings/scylla/cronsched"
 	"github.com/mowings/scylla/sched"
+	"github.com/mowings/scylla/ssh"
 	"log"
 	"regexp"
 	"strings"
@@ -25,6 +26,7 @@ const (
 type CommandRunReport struct {
 	CommandSpecified string
 	CommandRun       string
+	Error            string
 	StatusCode       int
 	StartTime        time.Time
 	EndTime          time.Time
@@ -63,6 +65,7 @@ type Job struct {
 	Pool            []string
 	PoolIndex       int
 	PoolMode        string
+	ConnectionCache map[string]*ssh.SshConnection
 }
 
 type JobList map[string]*Job
@@ -83,7 +86,7 @@ func (job *Job) Complete(r *RunReport) bool {
 		for _, run_report := range job.RunReports {
 			log.Printf("%s.%d.%s\n", run_report.JobName, run_report.RunId, run_report.Host)
 			for _, command_run_report := range run_report.CommandRunReports {
-				log.Printf("   %s (%s) %d\n", command_run_report.CommandSpecified, command_run_report.CommandRun, command_run_report.StatusCode)
+				log.Printf("   %s (%s) %s\n", command_run_report.CommandSpecified, command_run_report.CommandRun, command_run_report.Error)
 			}
 		}
 		job.PoolIndex += 1
@@ -96,6 +99,16 @@ func (job *Job) Complete(r *RunReport) bool {
 
 }
 
+func openConnection(keyfile string, host string, timeout int) (*ssh.SshConnection, error) {
+	auths := ssh.MakeKeyring([]string{keyfile})
+	var c ssh.SshConnection
+	err := c.Open(host, auths, timeout)
+	if err != nil {
+		return nil, err
+	}
+	return &c, err
+}
+
 func (job *Job) Run(run_report_chan chan *RunReport) {
 	if job.Running {
 		job.RunsQueued += 1
@@ -105,15 +118,36 @@ func (job *Job) Run(run_report_chan chan *RunReport) {
 	job.Running = true
 	host := qualifyHost(job.Pool[job.PoolIndex], job.Defaults.User, job.Defaults.Port)
 
-	go func() {
-		reports := make([]CommandRunReport, len(job.JobSpec.Command))
-		r := RunReport{job.Name, job.RunId, Succeeded, host, reports}
+	reports := make([]CommandRunReport, len(job.JobSpec.Command))
+	r := RunReport{job.Name, job.RunId, Succeeded, host, reports}
+	for index, command := range job.JobSpec.Command {
+		reports[index] = CommandRunReport{command, "", "", 0, time.Now(), time.Now()}
+	}
+	keyfile := job.Defaults.Keyfile
+	connection_timeout := job.Defaults.ConnectTimeout
 
-		for index, command := range job.JobSpec.Command {
-			started := time.Now()
-			log.Printf("%s.%d - running command \"%s\" on host %s\n", job.Name, job.RunId, command, host)
-			time.Sleep(2 * time.Second)
-			reports[index] = CommandRunReport{command, command, 0, started, time.Now()}
+	go func() {
+		conn, err := openConnection(keyfile, host, connection_timeout)
+		defer conn.Close()
+		if err != nil {
+			reports[0].Error = err.Error() // Just set first command to error on a failed connection
+		} else {
+			for _, report := range reports {
+				report.StartTime = time.Now()
+				log.Printf("%s.%d - running command \"%s\" on host %s\n", r.JobName, r.RunId, report.CommandSpecified, host)
+				stdout, stderr, err := conn.Run(report.CommandSpecified, 0, false)
+				if err != nil {
+					report.Error = err.Error()
+					report.StatusCode = -1
+				}
+				if stdout != nil {
+					log.Println("Stdout\n" + *stdout)
+				}
+				if stderr != nil {
+					log.Println("Sterr\n" + *stderr)
+				}
+				report.EndTime = time.Now()
+			}
 		}
 		run_report_chan <- &r
 	}()
