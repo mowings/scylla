7e06b4e650a7ebce095bcc53fd00d382e4c21d11
Michael Owings <mowings@turbosquid.com>
2015-05-20 14:13:18 -0500

    remove original loadchan push

--- main.go
+++ main.go
@@ -9,25 +9,18 @@ import (
 	"os"
 )
 
-func loadConfig(path string, load_chan chan string) (*config.Config, error) {
-	cfg, err := config.New(path)
-	if err != nil {
-		return nil, err
-	}
-	load_chan <- path
-	return cfg, nil
-
-}
-
 func main() {
+	var ctx web.Context
 	cfg_path := flag.String("config", "/etc/scylla.conf", "Config file path")
 	flag.Parse()
 	log.Printf("Starting scylla server...")
-	load_chan, _ := scheduler.Run()
-	cfg, err := loadConfig(*cfg_path, load_chan)
+	ctx.CfgPath = *cfg_path
+	ctx.LoadChan, _ = scheduler.Run()
+	cfg, err := config.New(ctx.CfgPath)
 	if err != nil {
 		log.Fatal("Unable to parse config file: " + err.Error())
 		os.Exit(-1)
 	}
-	web.Run(cfg.Web.Listen)
+	ctx.Config = *cfg
+	web.Run(&ctx)
 }
--- web/web.go
+++ web/web.go
@@ -3,14 +3,35 @@ package web
 import (
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
+	"github.com/mowings/scylla/config"
+	"net/http"
 )
 
-func Run(addr string) {
+type Context struct {
+	CfgPath  string
+	LoadChan chan string
+	Config   config.Config
+}
+
+func loadConfig(ctx Context) (*config.Config, error) {
+	cfg, err := config.New(ctx.CfgPath)
+	if err != nil {
+		return nil, err
+	}
+	ctx.LoadChan <- ctx.CfgPath
+	return cfg, nil
+}
+
+func Run(ctx *Context) {
 	server := martini.Classic()
 	server.Use(render.Renderer())
 	server.Get("/", func() string {
 		return "<h1>Scylla</h1>"
 	})
-	server.RunOnAddr(addr)
+	server.Put("/api/v1/config", func(req *http.Request, r render.Render) {
+		loadConfig(*ctx)
+	})
+
+	server.RunOnAddr(ctx.Config.Web.Listen)
 
 }
